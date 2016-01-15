2514d6580f8ec4b4f01229ffbfade2f00495e6e3
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2016-01-15 14:44:33 -0200

    Creation of the network done but still need to reorganize things

---
+++ src/App/App.java
@@ -0,0 +1,12 @@
+package App;
+
+import Base.NetConstructor;
+
+public class App {
+
+	public static void main(String[] args) {
+		NetConstructor net = new NetConstructor(15, 1, 0);
+		System.exit(0);
+	}
+
+}
--- src/Base/NetConstructor.java
+++ src/Base/NetConstructor.java
@@ -4,7 +4,7 @@ import Graph.*;
 
 public class NetConstructor {
 	// the pointer of the graph structure
-	private Graph g;
+	private Graph graph;
 	
 	
 	/**
@@ -20,11 +20,11 @@ public class NetConstructor {
 	public NetConstructor(int Inputs, int Outputs, int HiddenLayers){
 		// checking if the numbers are okay.
 		if(Outputs <= Inputs && HiddenLayers < Inputs){
+			int params[] =  {Inputs, HiddenLayers, Outputs};
 			// now check the numbers of hidden layers
 			if(HiddenLayers == 0){
-				this.createDeepNetwork();
+				this.createDeepNetwork(params);
 			}
-			int params[] =  {Inputs, Outputs, HiddenLayers};
 			this.createNetwork(params);
 		}
 	}
@@ -40,9 +40,40 @@ public class NetConstructor {
 	}
 
 
-	private void createDeepNetwork() {
-		// TODO Auto-generated method stub
+	private void createDeepNetwork(int[] params) {
+		this.graph = new Graph();
 		
+		// creates all the input nodes 
+		for(int i = 0; i < params[0]; i++){
+			this.graph.addNode("I: "+i);
+		}
+		
+		// creates all the hidden layers
+		for(int i = 0; i < params[0]-params[2]-1; i++){	
+			for(int j = 0; j < params[0]-1 - i; j++){
+				// creates all the nodes of a hidden layer
+				this.graph.addNode("H: "+i+"N: "+j);
+				for(int k = 0; k < params[0]; k++){
+					//creates all the joints of that node
+					if(i == 0){
+						this.graph.addDirectedVertex("I: "+k, "H: "+i+"N: "+j);
+					}
+					this.graph.addDirectedVertex("H: "+(i-1)+"N: "+k, "H: "+i+"N: "+j);
+				}
+			}
+		}
+		
+		// creates all the output nodes
+		for(int i = 0; i < params[2]; i++){
+			this.graph.addNode("O: "+i);
+			for(int k = 0; k < params[2]+1; k++){
+				//creates all the joints of that node
+				this.graph.addDirectedVertex("H: "+(params[0]-params[2]-2)+"N: "+k, "O: "+i);
+			}
+		}
+		
+		this.graph.CreateGraphVizFile("/Users/harielgiacomuzzi/Desktop/", true);
+		System.out.println("feitoria");
 	}
 	
 }
