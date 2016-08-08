5a06ecb6dc8874218c1a8ef1851be699a2b7d8a8
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2016-08-08 13:08:35 -0300

    remodeling

--- src/model/FUNCTION.java
+++
@@ -1,7 +0,0 @@
-package model;
-/*
- * You Can find more about these functions here: http://cs231n.github.io/neural-networks-1/#actfun
- */
-public enum FUNCTION{
-	Sigmoid, RectifiedLinear, HperbolicTangent
-}
